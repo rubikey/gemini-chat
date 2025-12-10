cdea80573677bad55cbeead7e84855b81750c355
rubikey <rubenboopego@gmail.com>
2025-12-10 14:58:09 +0100

    Add Math support

--- api/gemini.js
+++ api/gemini.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
   try {
     // Llamada a la API REST de Google Gemini
    
-// Usamos el modelo estándar 'gemini-pro' que es el más compatible
+// Usamos el modelo gemini-2.5-flash
 
 const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
     
